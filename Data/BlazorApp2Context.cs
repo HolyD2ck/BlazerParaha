4659ca4d6e92ff9096a115ba6bb9fa600ed39c7c
HolyD2ck <petranatolevic102@gmail.com>
2024-01-23 21:54:16 +0800

    Добавьте файлы проекта.

---
+++ Data/BlazorApp2Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorApp2.Models;
+
+    public class BlazorApp2Context : DbContext
+    {
+        public BlazorApp2Context (DbContextOptions<BlazorApp2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorApp2.Models.Student> Student { get; set; } = default!;
+    }
---
+++ Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp2.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string? Фамилия { get; set; }
+        public string? Имя { get; set; }
+        public string? Отчество { get; set; }
+        public int Рост { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime День_Рождения { get; set; }
+        public string? Группа { get; set; }
+        public string? Специальность { get; set; }
+        public string? Стипендия { get; set; }
+        public int? Возраст { get; set; }
+    }
+}
